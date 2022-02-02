eb630f4cdad9ab398d7d543f019befc5bcfb4193
Alerio Reyhan <alerioreyhan127@gmail.com>
2022-02-02 08:26:19 +0700

    finished project

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authentication");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middlewares/authMiddleware");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+const dbURI =
+  "mongodb+srv://aerweka:testing123@cluster0.ul8wj.mongodb.net/node-jwt";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log("Server running");
+    })
+  )
   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.use(authRoutes);
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+require("dotenv").config();
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check jwt exists n is verified
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.local.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
